a3f6ab4f4cfd2ebc81f7935d75fcb61a769d0407
joseMarciano <marcianojosepaulo@gmail.com>
2022-04-26 20:10:31 -0300

    chore: create MongoConfig

---
+++ src/main/java/com/app/chat/infra/db/MongoConfig.java
@@ -0,0 +1,30 @@
+package com.app.chat.infra.db;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@Configuration
+@EnableMongoRepositories(basePackages = "com.app.chat.infra.repositories")
+public class MongoConfig {
+
+    @Bean
+    public MongoClient mongo() {
+        ConnectionString connectionString = new ConnectionString("mongodb://localhost:27017/chat");
+        MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .build();
+
+        return MongoClients.create(mongoClientSettings);
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate() {
+        return new MongoTemplate(mongo(), "chat");
+    }
+}
