df53282a8bc5b8603eabc80980a5567d79a0d08e
joseMarciano <marcianojosepaulo@gmail.com>
2022-04-27 16:43:11 -0300

    refactor: add allArgsConstructor

--- src/main/java/com/app/chat/entities/usecases/user/AddUserModel.java
+++ src/main/java/com/app/chat/entities/usecases/user/AddUserModel.java
@@ -1,5 +1,6 @@
 package com.app.chat.entities.usecases.user;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,6 +8,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @Builder
+@AllArgsConstructor
 public class AddUserModel {
     private String email;
     private String name;
